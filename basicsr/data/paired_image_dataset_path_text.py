025028eaa5b2fd027d772697a548827a54c1229e
Fradeet <dgyeotoft@outlook.com>
2023-06-05 14:15:40 +0800

    clean unuse import

--- basicsr/data/paired_image_dataset_path_text.py
+++ basicsr/data/paired_image_dataset_path_text.py
@@ -1,7 +1,7 @@
 from torch.utils import data as data
 from torchvision.transforms.functional import normalize
 
-from basicsr.data.data_util import paired_paths_from_folder, paired_paths_from_lmdb, paired_paths_from_meta_info_file, paired_paths_from_text
+from basicsr.data.data_util import paired_paths_from_text
 from basicsr.data.transforms import augment, paired_random_crop
 from basicsr.utils import FileClient, bgr2ycbcr, imfrombytes, img2tensor
 from basicsr.utils.registry import DATASET_REGISTRY
